c13088b8448c51f987ac0af53b241fd052ab7ddd
Miguel Serrano <mlsv89@gmail.com>
2016-05-23 17:12:29 +0200

    require authentication to / route

--- plugins/controllers/index.js
+++ plugins/controllers/index.js
@@ -1,13 +1,8 @@
 'use strict';
 
 module.exports = function handler(request, reply) {
-  const userData = {};
-  // Already logged in?
   if (request.auth.isAuthenticated) {
-    // User logged in show user links.
-    userData.username = request.auth.credentials.username;
-    userData.email = request.auth.credentials.email;
-    return reply.redirect('/dashboard');
+    return reply.view('index');
   }
-  return reply.view('index');
+  return reply.redirect('/login');
 };
