f751680d4ada6bfb439a71cc4ce7498d8a498d2e
Miguel Serrano <mlsv89@gmail.com>
2016-05-23 17:19:53 +0200

    changed index controller to simple view handler

--- plugins/controllers/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function handler(request, reply) {
-  if (request.auth.isAuthenticated) {
-    return reply.view('index');
-  }
-  return reply.redirect('/login');
-};
