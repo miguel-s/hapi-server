d9e6b8946b0ca2b4169f90c72304bd09d153aa2b
Miguel Serrano <mlsv89@gmail.com>
2016-05-24 12:12:06 +0200

    added encryption to login and signup

--- plugins/controllers/login.js
+++ plugins/controllers/login.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const aguid = require('aguid');
+const bcrypt = require('bcryptjs');
 
 module.exports = function handler(request, reply) {
   let account = {};
@@ -15,22 +16,36 @@ module.exports = function handler(request, reply) {
       email: request.payload.email,
     });
   }
+
   request.server.app.db.get('SELECT * FROM Users WHERE email = ?', request.payload.email,
     (err, row) => {
       if (err) return reply(err);
-      if (!row || row.password !== request.payload.password) {
+
+      if (!row) {
         return reply.view('login', {
           message: 'Invalid email or password',
           email: request.payload.email,
         });
       }
-      account = Object.assign(account, row, { scope: row.scope.split(', ') });
-      if (account.password) delete account.password; // don't save pw in cookie
-      const sid = String(aguid());
-      request.server.app.cache.set(sid, { account }, 0, (err) => {
+
+      bcrypt.compare(request.payload.password, row.password, (err, res) => {
         if (err) return reply(err);
-        request.cookieAuth.set({ sid });
-        return reply.redirect('/admin');
+
+        if (!res) {
+          return reply.view('login', {
+            message: 'Invalid email or password',
+            email: request.payload.email,
+          });
+        }
+
+        account = Object.assign(account, row, { scope: row.scope.split(', ') });
+        if (account.password) delete account.password; // don't save pw in cookie
+        const sid = String(aguid());
+        request.server.app.cache.set(sid, { account }, 0, (err) => {
+          if (err) return reply(err);
+          request.cookieAuth.set({ sid });
+          return reply.redirect('/admin');
+        });
       });
     }
   );
--- plugins/controllers/signup.js
+++ plugins/controllers/signup.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const aguid = require('aguid');
+const bcrypt = require('bcryptjs');
 
 module.exports = function handler(request, reply, source, error) {
   let account = {};
@@ -9,22 +10,17 @@ module.exports = function handler(request, reply, source, error) {
     return reply.redirect('/admin');
   }
 
-  // const key = error.data.details[0].path;
-  // const err = {};
-  // err[key] = {
-  //   class: 'input-error', // css class
-  //   message: error.data.details[0].message, // Joi error message
-  // };
-
   if (!request.payload.email || !request.payload.password) {
     return reply.view('signup', {
       message: 'Missing email or password',
       email: request.payload.email,
     });
   }
+
   request.server.app.db.get('SELECT * FROM Users WHERE email = ?', request.payload.email,
     (err, row) => {
       if (err) return reply(err);
+
       if (row) {
         return reply.view('signup', {
           message: 'User already exists',
@@ -32,32 +28,36 @@ module.exports = function handler(request, reply, source, error) {
         });
       }
 
-      // TODO:
-      // add Joi validation for payload
-      account = {
-        id: aguid(request.payload.email),
-        username: null,
-        password: request.payload.password,
-        email: request.payload.email,
-        scope: 'user',
-      };
-      request.server.app.db.run(
-        'INSERT INTO Users (id, username, password, email, scope) VALUES (?, ?, ?, ?, ?)',
-        account.id,
-        account.username,
-        account.password,
-        account.email,
-        account.scope,
-        (err) => {
+      bcrypt.genSalt(12, (err, salt) => {
+        if (err) return reply(err);
+        bcrypt.hash(request.payload.password, salt, (err, hash) => {
           if (err) return reply(err);
-          const sid = String(aguid());
-          request.server.app.cache.set(sid, { account }, 0, (err) => {
-            if (err) return reply(err);
-            request.cookieAuth.set({ sid });
-            return reply.redirect('/admin');
-          });
-        }
-      );
+          account = {
+            id: aguid(request.payload.email),
+            username: null,
+            password: hash,
+            email: request.payload.email,
+            scope: 'user',
+          };
+          request.server.app.db.run(
+            'INSERT INTO Users (id, username, password, email, scope) VALUES (?, ?, ?, ?, ?)',
+            account.id,
+            account.username,
+            account.password,
+            account.email,
+            account.scope,
+            (err) => {
+              if (err) return reply(err);
+              const sid = String(aguid());
+              request.server.app.cache.set(sid, { account }, 0, (err) => {
+                if (err) return reply(err);
+                request.cookieAuth.set({ sid });
+                return reply.redirect('/admin');
+              });
+            }
+          );
+        });
+      });
     }
   );
 };
