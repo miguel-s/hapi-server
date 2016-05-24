7941cc92f16722998642625921125433145f4505
Miguel Serrano <mlsv89@gmail.com>
2016-05-24 09:25:01 +0200

    tls redirection only in staging or production environment

--- plugins/tls.js
+++ plugins/tls.js
@@ -6,14 +6,16 @@ exports.register = (server, options, next) => {
   const web = server.select('web');
   // const webTls = server.select('web-tls');
 
-  // TLS everything
-  web.ext('onRequest', (request, reply) => {
-    if (request.headers['x-forwarded-proto'] === 'http') {
-      return reply.redirect(`https://${request.headers.host}${request.url.path}`).permanent();
-    }
-    return reply.continue();
-  });
-  return next();
+  if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    // TLS everything
+    web.ext('onRequest', (request, reply) => {
+      if (request.headers['x-forwarded-proto'] === 'http') {
+        return reply.redirect(`https://${request.headers.host}${request.url.path}`).permanent();
+      }
+      return reply.continue();
+    });
+    return next();
+  }
 };
 
 exports.register.attributes = {
