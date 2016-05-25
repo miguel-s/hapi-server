3fe938128daf5dc543086114444ddd3667ec62a5
Miguel Serrano <mlsv89@gmail.com>
2016-05-25 09:48:37 +0200

    added server settings for controlling server behaviour at runtime

--- start.js
+++ start.js
@@ -2,6 +2,7 @@
 
 const Hoek = require('hoek');
 const Server = require('./server.js');
+const settings = require('./settings.js');
 
 const internals = {};
 internals.manifest = require('./manifest');
@@ -12,6 +13,8 @@ Server.init(internals.manifest, internals.composeOptions, (err, server) => {
 
   const web = server.select('web');
   // const webTls = server.select('web-tls');
+
+  web.app.settings = internals.settings = settings;
 
   console.log(`Web server started at: ${web.info.uri}`);
   // console.log(`WebTLS server started at: ${webTls.info.uri}`);
