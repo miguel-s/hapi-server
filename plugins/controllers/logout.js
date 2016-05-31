caaa56856e899e450f5f7dc1edb8e98af14cbe08
Miguel Serrano <mlsv89@gmail.com>
2016-05-31 14:01:07 +0200

    added prefix

--- plugins/controllers/logout.js
+++ plugins/controllers/logout.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function handler(request, reply) {
+  const prefix = request.route.realm.modifiers.route.prefix;
   request.cookieAuth.clear();
-  return reply.redirect('/');
+  return reply.redirect(prefix);
 };
