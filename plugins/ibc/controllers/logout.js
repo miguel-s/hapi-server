cb58c72d6bb5f94c960bae2737be34b5ca763c89
Miguel Serrano <mlsv89@gmail.com>
2016-06-03 13:31:43 +0200

    moved plugins to their own repository

--- plugins/ibc/controllers/logout.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function handler(request, reply) {
-  request.cookieAuthIbc.clear();
-  return reply.redirect('/');
-};
--- plugins/wantit/controllers/logout.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function handler(request, reply) {
-  request.cookieAuthWantit.clear();
-  return reply.redirect('/');
-};
