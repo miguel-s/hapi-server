eeb797522aedc28c968461c7cdfbcd0f3967da18
Miguel Serrano <mlsv89@gmail.com>
2016-06-29 11:39:39 +0200

    cleanup

--- start.js
+++ start.js
@@ -15,10 +15,8 @@ Server.init(internals.manifest, internals.composeOptions, (err, server) => {
   server.app.settings = internals.settings;
 
   const admin = server.select('admin');
-  const wantit = server.select('wantit');
   const ibc = server.select('ibc');
 
   console.log(`Admin connection started at: ${admin.info.uri}`);
-  console.log(`Wantit connection started at: ${wantit.info.uri}`);
   console.log(`Ibc connection started at: ${ibc.info.uri}`);
 });
